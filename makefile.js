732c5900dcc84b67631343fb8812d9aded0b0e7c
Nick <nick@polyforest.com>
2021-01-18 09:06:44 -0600

    Add CI auto version bump

--- makefile.js
+++ makefile.js
@@ -1,10 +1,16 @@
 import path from 'path';
-import {exec} from 'child_process';
+import {execSync} from 'child_process';
 import * as fs from 'fs';
+import semver from 'semver';
 
 'use strict';
 
-console.log(process.env.npm_package_version);
+/**
+ * Sets the git user.name to GITHUB_ACTOR.
+ */
+function setGitUser() {
+  execSync(`git config user.name "${process.env.GITHUB_ACTOR}"`);
+}
 
 /**
  * Gets a path to an executable in node_modules/.bin
@@ -15,36 +21,64 @@ function getBinFile(command) {
   return path.join('node_modules', '.bin', command);
 }
 
-const emptyCallback = () => {
-};
-
-/**
- * Executes a command with logging.
- * @param {string} command
- * @param {function} onSuccess
- */
-function cmd(command, onSuccess = emptyCallback) {
-  console.log(command);
-  exec(command,
-    (error, stdout, stderr) => {
-      if (error) console.error(error, stderr);
-      if (stdout.length > 0) {
-        console.log(stdout);
-      }
-      if (!error) onSuccess();
-    });
-}
-
 const args = process.argv.slice(2);
-
 const targets = {};
 const version = process.env.npm_package_version;
 
 targets.doc = () => {
-  cmd(`${getBinFile('jsdoc')} -c jsdoc.conf.json -d jsdocs/v${version}`, () => {
-    const data = `<script>window.location.replace("./v${version}/");</script>`;
-    fs.writeFileSync('jsdocs/index.html', data);
-  });
+  execSync(`${getBinFile('jsdoc')} -c jsdoc.conf.json -d jsdocs/v${version}`);
+  const data = `<script>window.location.replace("./v${version}/");</script>`;
+  fs.writeFileSync('jsdocs/index.html', data);
+};
+
+targets.bumpVersion = () => {
+  setGitUser();
+  execSync(`git fetch --all --tags`);
+  const tagsStr = execSync(`git tag -l`).toString();
+  const tags = tagsStr.trim().split('\n');
+  let lastVersion = null;
+  let lastTag = null;
+  for (let i = tags.length; i >= 0; i--) {
+    const iVer = semver.valid(tags[i]);
+    if (iVer != null && (lastVersion == null || semver.gt(iVer, lastVersion))) {
+      lastVersion = iVer;
+      lastTag = tags[i];
+    }
+  }
+  if (lastVersion == null) {
+    console.log('No tags found, no need to bump version.');
+    return;
+  }
+  if (semver.gt(lastVersion, version)) {
+    console.error(`Current version '${version}' should not be less than last ` +
+    `version '${lastVersion}'`);
+    process.exit(-1);
+  }
+  console.log(`Last tagged version: ${lastVersion}`);
+
+  const log = execSync(`git log ${lastTag}..HEAD`).toString();
+  let type;
+  if (/\bmajor\b/.test(log)) type = 'major';
+  else if (/\bminor\b/.test(log)) type = 'minor';
+  else type = 'patch';
+  const newVersion = semver.inc(lastVersion, type);
+  console.log(`Incrementing last tagged version by '${type}'. New minimum ` +
+    `version is ${newVersion}.`);
+  if (semver.gt(newVersion, version)) {
+    const msg = `Bumping ${type} version. New version: ${newVersion}`;
+    execSync(`npm version ${type} --git-tag-version=false`);
+    execSync(`git add package.json package-lock.json`);
+    execSync(`git commit -m "ci: ${msg}"`);
+    execSync(`git push`);
+  } else {
+    console.log(`Current version ${version} is greater or equal.`);
+  }
+};
+
+targets.pushVersionTag = () => {
+  setGitUser();
+  execSync(`git tag v${version}`);
+  execSync(`git push --tags`);
 };
 
 if (args.length === 0) throw new Error('No target specified');
