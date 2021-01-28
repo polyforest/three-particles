b7929b274648aaa64782f8ad37c54fa3e921b31b
Nick <589193+nbilyk@users.noreply.github.com>
2021-01-28 08:11:49 -0600

    Add randomFromRange tests

--- test/model.js
+++ test/model.js
@@ -2,13 +2,33 @@ import * as model from '../src/model.js';
 import assert from 'assert';
 
 describe('model', () => {
-  describe('randomFromRange', () => {
-    const range1 = /** @type {model.RangeVo} */ {min: 23, max: 43};
+  describe('randomFromRange-linear', () => {
+    const range = /** @type {model.RangeVo} */ {min: 23, max: 43};
     it('randomFromRange should return min when rng() provides 0.0', () => {
-      assert.strictEqual(model.randomFromRange(range1, ()=>0), 23);
+      assert.strictEqual(model.randomFromRange(range, () => 0), 23);
     });
     it('randomFromRange should return max when rng() provides 1.0', () => {
-      assert.strictEqual(model.randomFromRange(range1, ()=>1), 43);
+      assert.strictEqual(model.randomFromRange(range, () => 1), 43);
+    });
+    it('randomFromRange should return mid when rng() provides 0.5', () => {
+      assert.strictEqual(model.randomFromRange(range, () => 0.5), 33);
+    });
+  });
+  describe('randomFromRange-pow2In', () => {
+    const range = /** @type {model.RangeVo} */ ({
+      min: 23,
+      max: 63,
+      ease: 'pow2In',
+    });
+    it('randomFromRange should return min when rng() provides 0.0', () => {
+      assert.strictEqual(model.randomFromRange(range, () => 0), 23);
+    });
+    it('randomFromRange should return max when rng() provides 1.0', () => {
+      assert.strictEqual(model.randomFromRange(range, () => 1), 63);
+    });
+    it('randomFromRange should return mid when rng() provides 0.5', () => {
+      // 23 + (63 - 23) * (0.5 * 0.5)
+      assert.strictEqual(model.randomFromRange(range, () => 0.5), 33);
     });
   });
 });
