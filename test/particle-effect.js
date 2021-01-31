bbfccb9390b7bf4f918aa510702cdfaa654a4e7e
nbilyk <589193+nbilyk@users.noreply.github.com>
2021-01-31 09:28:51 -0600

    Remove log

--- test/particle-effect.js
+++ test/particle-effect.js
@@ -1,12 +1,10 @@
 import assert from 'assert';
 import {ParticleEffect} from '../src/particle-effect.js';
-import {AdditiveBlending} from 'three';
 import {sanitizeEmitter, sanitizeParticleEffect} from '../src/model.js';
 
 describe('ParticleEffect', () => {
   describe('constructor', () => {
     it('Constructor should accept an emitters list.', () => {
-      console.log(AdditiveBlending);
       const pE = new ParticleEffect(sanitizeParticleEffect({
         emitters: [
           sanitizeEmitter({}),
