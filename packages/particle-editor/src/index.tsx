f5020e9163cd9a955211447f5a851f8f316cf294
nbilyk <nbilyk@mac.lan>
2025-08-16 09:51:23 -0700

    chore: add mui components

--- packages/particle-editor/src/index.tsx
+++ packages/particle-editor/src/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.scss'
+import {
+    Card,
+    CardContent,
+    CardActions,
+    Button,
+    Typography,
+    Box,
+} from '@mui/material'
 
 const App: React.FC = () => {
     return (
         <div className="app-container">
-            <h1>Hello World from Particle Editor!</h1>
+            <Box sx={{ minWidth: 275, margin: 2 }}>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5" component="div" gutterBottom>
+                            Particle Editor
+                        </Typography>
+                        <Typography variant="body2">
+                            Welcome to the Particle Editor! This is a
+                            Material-UI example component. You can use this card
+                            as a container for particle system controls.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Create Particle System</Button>
+                        <Button size="small">Reset</Button>
+                    </CardActions>
+                </Card>
+            </Box>
         </div>
     )
 }
 
-console.log('Hi')
 const container = document.getElementById('root')
 if (container) {
     const root = createRoot(container)
