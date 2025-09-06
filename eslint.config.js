ddf57b368e6d92aaa46393c0b5567410309fc839
nbilyk <nbilyk@Nicholass-MacBook-Pro.local>
2025-09-06 11:21:18 -0700

    chore: ignore *.config.ts for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 
 export default [
     {
-        ignores: ['**/dist/**', '**/build/**', '**/*.js'],
+        ignores: ['**/dist/**', '**/build/**', '**/*.js', '**/*.config.ts'],
     },
     ...tseslint.config(
         eslint.configs.recommended,
